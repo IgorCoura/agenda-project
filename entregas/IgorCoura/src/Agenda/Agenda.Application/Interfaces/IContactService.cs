9b4dfe76c4497ed7defe3bd5b14bb2ba1777ec86
Igor de Brito Coura <igor-coura@hotmail.com>
2022-07-04 11:25:52 -0300

    reactor: Reorganizando o código.

--- entregas/IgorCoura/src/Agenda/Agenda.Application/Interfaces/IContactService.cs
+++ entregas/IgorCoura/src/Agenda/Agenda.Application/Interfaces/IContactService.cs
@@ -6,12 +6,12 @@ namespace Agenda.Application.Interfaces
 {
     public interface IContactService
     {
-        Task<ContactModel> Register(CreateContactModel contact, int? userId = null);
-        Task<ContactModel> Edit(UpdateContactModel contactModel, int? userId = null);
+        Task<ContactModel> Register(CreateContactModel contact, int userId);
+        Task<ContactModel> Edit(UpdateContactModel contactModel, int userId);
         Task<ContactModel> RecoverById(int id, int? userId = null);
         Task<IEnumerable<ContactModel>> RecoverAll(int? userId = null);
         Task<IEnumerable<ContactModel>> Recover(ContactParams query, int? userId = null);
-        Task<ContactModel> Remove(int id, int? userId = null);
-        Task<ContactModel> RemovePhone(int id, int? userId = null);
+        Task<ContactModel> Remove(int id, int userId);
+        Task<ContactModel> RemovePhone(int id, int userId);
     }
 }
