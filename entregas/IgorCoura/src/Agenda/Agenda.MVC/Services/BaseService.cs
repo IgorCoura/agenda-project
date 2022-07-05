f749282a78b404617d82f6df5efeb65af735df5c
Igor de Brito Coura <igor-coura@hotmail.com>
2022-07-04 22:03:18 -0300

    feat: Criando sistema de notificacao de erros para o usuario.

---
+++ entregas/IgorCoura/src/Agenda/Agenda.MVC/Services/BaseService.cs
@@ -0,0 +1,30 @@
+using Agenda.MVC.Interfaces;
+using Agenda.MVC.Notifications;
+using Agenda.MVC.ViewModel;
+using Flurl.Http;
+
+namespace Agenda.MVC.Services
+{
+    public class BaseService
+    {
+        private readonly INotificator _notificador;
+
+        protected BaseService(INotificator notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected void Notify(string mensagem)
+        {
+            _notificador.Handle(new Notification(mensagem));
+        }
+
+        protected void Notify(IEnumerable<string> mensagens)
+        {
+            var notifications = mensagens.Select(x => new Notification(x));
+            _notificador.Handle(notifications);
+        }
+    }
+}
+
+
