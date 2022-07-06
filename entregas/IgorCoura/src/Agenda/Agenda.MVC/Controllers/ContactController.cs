7b5a3cfc4cef6802263e04a05365d6eb43e4450d
Igor de Brito Coura <igor-coura@hotmail.com>
2022-07-06 19:13:59 -0300

    feat: Melhorando o visual do Contact e do Login.

--- entregas/IgorCoura/src/Agenda/Agenda.MVC/Controllers/ContactController.cs
+++ entregas/IgorCoura/src/Agenda/Agenda.MVC/Controllers/ContactController.cs
@@ -1,9 +1,11 @@
 using Agenda.MVC.Interfaces;
 using Agenda.MVC.Params;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Agenda.MVC.Controllers
 {
+    [Authorize]
     public class ContactController : Controller
     {
         private readonly IContactService _contactService;
--- entregas/IgorCoura/src/Agenda/Agenda.MVC/ViewModel/LoginViewModel.cs
+++ entregas/IgorCoura/src/Agenda/Agenda.MVC/ViewModel/LoginViewModel.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Agenda.MVC.ViewModel
 {
     public class LoginViewModel
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
